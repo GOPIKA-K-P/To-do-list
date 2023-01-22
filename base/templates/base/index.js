f2a7a4d4d32aaf200f1ca3cacaf884f5c55e4b1a
Gopika <kamalgopika54@gmail.com>
2023-01-22 14:13:52 +0530

    first commit

---
+++ base/templates/base/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgTEo2rM2LYr4NyUIPjKzYu3quE6LCzpE",
+    authDomain: "to-do-list-b2c2c.firebaseapp.com",
+    projectId: "to-do-list-b2c2c",
+    storageBucket: "to-do-list-b2c2c.appspot.com",
+    messagingSenderId: "523324289317",
+    appId: "1:523324289317:web:0b4cd7186ab9bcafd1eacc",
+    measurementId: "G-NNBCWSXL4Q"
+  };
+
